770ce049c6fce67e9100cdfff01576ea7545044c
Victor Mattos <5757883+vicmattos@users.noreply.github.com>
2022-03-11 17:57:19 -0300

    feat: ensure AddPurchaseController returns 400 if no currency is provided

--- src/reimbursement_manager/presentation/controller/purchase/add_purchase.py
+++ src/reimbursement_manager/presentation/controller/purchase/add_purchase.py
@@ -3,7 +3,13 @@ from typing import Any
 
 class AddPurchaseController():
     def handle(self, request: Any) -> Any:
-        return {
-            'status_code': 400,
-            'body': "Missing param: amount"
-        }
+        if request['body'].get('amount') is None:
+            return {
+                'status_code': 400,
+                'body': "Missing param: amount"
+            }
+        if request['body'].get('currency') is None:
+            return {
+                'status_code': 400,
+                'body': "Missing param: currency"
+            }
