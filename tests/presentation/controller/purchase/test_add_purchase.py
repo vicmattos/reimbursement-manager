91fb7e6218d0db3d4155601475c14746abe45d38
Victor Mattos <5757883+vicmattos@users.noreply.github.com>
2022-03-12 11:15:26 -0300

    refactor: create sut (System Under Test) for test_add_purchase as a fixture

--- tests/presentation/controller/purchase/test_add_purchase.py
+++ tests/presentation/controller/purchase/test_add_purchase.py
@@ -2,49 +2,51 @@
 from decimal import Decimal
 from datetime import date
 
+import pytest
+
 from reimbursement_manager.presentation.controller.purchase import AddPurchaseController
 from reimbursement_manager.presentation.protocols.http import HttpResponse, HttpRequest
 
-class ConfirmRequiredFieldsTests():
 
-    def make_sut(self):
-        return AddPurchaseController()
+@pytest.fixture
+def sut():
+    return AddPurchaseController()
 
-    def test_return_400_if_no_amount_is_provided(self):
-        sut = self.make_sut()
-        http_request = HttpRequest(
-            body = dict(
-                # amount = Decimal(10.00),
-                currency = "BRL",
-                date = date.today()
-            )
+
+def test_return_400_if_no_amount_is_provided(sut):
+    http_request = HttpRequest(
+        body = dict(
+            # amount = Decimal(10.00),
+            currency = "BRL",
+            date = date.today()
         )
-        http_response: HttpResponse = sut.handle(http_request)
-        assert http_response.status_code == 400
-        assert http_response.body.get('message') == "Missing param: amount"
+    )
+    http_response: HttpResponse = sut.handle(http_request)
+    assert http_response.status_code == 400
+    assert http_response.body.get('message') == "Missing param: amount"
 
-    def test_return_400_if_no_currency_is_provided(self):
-        sut = self.make_sut()
-        http_request = HttpRequest(
-            body = dict(
-                amount = Decimal(10.00),
-                # currency = "BRL",
-                date = date.today()
-            )
-        )
-        http_response: HttpResponse = sut.handle(http_request)
-        assert http_response.status_code == 400
-        assert http_response.body.get('message') == "Missing param: currency"
 
-    def test_return_400_if_no_date_is_provided(self):
-        sut = self.make_sut()
-        http_request = HttpRequest(
-            body = dict(
-                amount = Decimal(10.00),
-                currency = "BRL",
-                # date = date.today()
-            )
+def test_return_400_if_no_currency_is_provided(sut):
+    http_request = HttpRequest(
+        body = dict(
+            amount = Decimal(10.00),
+            # currency = "BRL",
+            date = date.today()
         )
-        http_response: HttpResponse = sut.handle(http_request)
-        assert http_response.status_code == 400
-        assert http_response.body.get('message') == "Missing param: date"
+    )
+    http_response: HttpResponse = sut.handle(http_request)
+    assert http_response.status_code == 400
+    assert http_response.body.get('message') == "Missing param: currency"
+
+
+def test_return_400_if_no_date_is_provided(sut):
+    http_request = HttpRequest(
+        body = dict(
+            amount = Decimal(10.00),
+            currency = "BRL",
+            # date = date.today()
+        )
+    )
+    http_response: HttpResponse = sut.handle(http_request)
+    assert http_response.status_code == 400
+    assert http_response.body.get('message') == "Missing param: date"
