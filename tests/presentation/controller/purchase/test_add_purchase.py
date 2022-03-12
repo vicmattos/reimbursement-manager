37e9e8486131f96cdf6577b8beeb9e166baa1142
Victor Mattos <5757883+vicmattos@users.noreply.github.com>
2022-03-11 21:29:41 -0300

    test: ensure AddPurchaseController returns 400 if no date is provided

--- tests/presentation/controller/purchase/test_add_purchase.py
+++ tests/presentation/controller/purchase/test_add_purchase.py
@@ -30,3 +30,16 @@ def test_return_400_if_no_currency_is_provided():
     http_response: HttpResponse = sut.handle(http_request)
     assert http_response.status_code == 400
     assert http_response.body.get('message') == "Missing param: currency"
+
+def test_return_400_if_no_date_is_provided():
+    sut = AddPurchaseController()
+    http_request = HttpRequest(
+        body = dict(
+            amount = Decimal(10.00),
+            currency = "BRL",
+            # date = date.today()
+        )
+    )
+    http_response: HttpResponse = sut.handle(http_request)
+    assert http_response.status_code == 400
+    assert http_response.body.get('message') == "Missing param: date"
