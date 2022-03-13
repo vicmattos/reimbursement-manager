8d1e4473580580e7693a4b134597630cdaea67bc
Victor Mattos <5757883+vicmattos@users.noreply.github.com>
2022-03-13 13:38:20 -0300

    refactor: extract AddPurchaseController.handle validation code to specialized methods

--- reimbursement_manager/presentation/controller/purchase/add_purchase.py
+++ reimbursement_manager/presentation/controller/purchase/add_purchase.py
@@ -1,3 +1,4 @@
+from typing import List
 
 from reimbursement_manager.presentation.helpers.http_helper import invalid_request_response, internal_error_response
 from reimbursement_manager.presentation.protocols import HttpResponse, HttpRequest, Controller, CurrencyValidator
@@ -9,21 +10,10 @@ class AddPurchaseController(Controller):
         self._currency_validator = currency_validator
 
     def handle(self, request: HttpRequest) -> HttpResponse:
-
         try:
-            for required_field in ['amount', 'currency', 'date']:
-                field_value = request.body.get(required_field, None)
-                if field_value is None:
-                    raise MissingParamError(param_name=required_field)
-
-            amount = request.body.get('amount')
-            if amount <= 0:
-                raise InvalidParamError(param_name='amount')
-
-            currency = request.body.get('currency')
-            is_valid = self._currency_validator.is_valid(currency)
-            if not is_valid:
-                raise InvalidParamError(param_name='currency')
+            self._validate_required_fields(request.body, ['amount', 'currency', 'date'])
+            self._validate_amount(request.body.get('amount'))
+            self._validate_currency(request.body.get('currency'))
 
         except (MissingParamError, InvalidParamError) as err:
             response = invalid_request_response(message=err.message)
@@ -31,3 +21,21 @@ class AddPurchaseController(Controller):
             response = internal_error_response()
 
         return response
+
+
+    def _validate_required_fields(self, body: dict, required_fields: List[str]) -> None:
+        for required_field in required_fields:
+            field_value = body.get(required_field, None)
+            if field_value is None:
+                raise MissingParamError(param_name=required_field)
+
+
+    def _validate_amount(self, amount: int) -> None:
+        if amount <= 0:
+            raise InvalidParamError(param_name='amount')
+
+
+    def _validate_currency(self, currency: str) -> None:
+        is_valid = self._currency_validator.is_valid(currency)
+        if not is_valid:
+            raise InvalidParamError(param_name='currency')
