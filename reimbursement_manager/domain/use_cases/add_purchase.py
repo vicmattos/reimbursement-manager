cf7f190e6c7d02aeed4dd293839de0f9254bafe2
Victor Mattos <5757883+vicmattos@users.noreply.github.com>
2022-03-14 11:18:20 -0300

    refactor: create encapsulation for adding a purchase as AddPurchaseModel

--- reimbursement_manager/domain/use_cases/add_purchase.py
+++ reimbursement_manager/domain/use_cases/add_purchase.py
@@ -2,8 +2,40 @@ from datetime import date
 from decimal import Decimal
 from abc import ABC, abstractmethod
 
+from reimbursement_manager.domain.model.purchase import PurchaseModel
+
+class AddPurchaseModel():
+
+    def __init__(self, amount, currency, date):
+        self._amount = amount
+        self._currency = currency
+        self._date = date
+
+    @property
+    def amount(self) -> Decimal:
+        return self._amount # pragma: no cover
+
+    @property
+    def currency(self) -> str:
+        return self._currency # pragma: no cover
+
+    @property
+    def date(self) -> date:
+        return self._date # pragma: no cover
+
+    def __eq__(self, other):
+        if not isinstance(other, AddPurchaseModel):
+            return False
+
+        same_amount = self._amount == other._amount
+        same_currency = self._currency == other._currency
+        same_date = self._date == other._date
+
+        return True if (same_amount and same_currency and same_date) else False
+
+
 class AddPurchase(ABC):
 
     @abstractmethod
-    def add(self, amount: Decimal, currency: str, date: date) -> None:
-        raise NotImplementedError()
+    def add(self, add_purchase_model: AddPurchaseModel) -> PurchaseModel:
+        raise NotImplementedError() # pragma: no cover
---
+++ tests/domain/use_cases/test_add_purchase_case.py
@@ -0,0 +1,23 @@
+from decimal import Decimal
+from datetime import date
+
+import pytest
+
+from reimbursement_manager.domain.use_cases.add_purchase import AddPurchaseModel
+
+
+@pytest.fixture
+def sut():
+    amount = Decimal(10.00)
+    currency = 'BRL'
+    purchase_date = date.today()
+    return AddPurchaseModel(amount, currency, purchase_date)
+
+
+def test_result_true_comparing_with_correct_values(sut):
+    other = AddPurchaseModel(
+        amount=Decimal(10.00),
+        currency='BRL',
+        date=date.today()
+    )
+    assert sut == other
