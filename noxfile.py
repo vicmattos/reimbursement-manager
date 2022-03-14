3fd02953c6767c8269d45b1a0d4cf54b498dd540
Victor Mattos <5757883+vicmattos@users.noreply.github.com>
2022-03-14 18:43:45 -0300

    chore: add flake8 linting through nox

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,18 @@
 import nox
 
+
+locations = "reimbursement_manager", "tests", "noxfile.py"
+
+
 @nox.session()
 def tests(session):
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
+
+
+@nox.session()
+def flake8(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
