4a26218b73f18493eec249ad4522a94fd437378c
Victor Mattos <5757883+vicmattos@users.noreply.github.com>
2022-03-16 08:53:38 -0300

    fix: correct return type hint as optional

--- reimbursement_manager/presentation/protocols/http.py
+++ reimbursement_manager/presentation/protocols/http.py
@@ -1,8 +1,9 @@
+from typing import Optional
 
 
 class HttpResponse():
 
-    def __init__(self, status_code: int, body: dict = None):
+    def __init__(self, status_code: int, body: Optional[dict]):
         self._status_code = status_code
         self._body = body
 
@@ -11,7 +12,7 @@ class HttpResponse():
         return self._status_code
 
     @property
-    def body(self) -> dict:
+    def body(self) -> Optional[dict]:
         return self._body
 
 
