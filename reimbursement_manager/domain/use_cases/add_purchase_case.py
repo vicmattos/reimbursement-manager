00bc2c6a5cdd3b748df1a778911d7c841b5a428f
Victor Mattos <5757883+vicmattos@users.noreply.github.com>
2022-03-16 15:07:29 -0300

    refactor: change AddPurchaseModel to be a @dataclass

--- reimbursement_manager/domain/use_cases/add_purchase_case.py
+++ reimbursement_manager/domain/use_cases/add_purchase_case.py
@@ -1,38 +1,16 @@
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from datetime import date
 from decimal import Decimal
 
 from reimbursement_manager.domain.model.purchase_model import PurchaseModel
 
 
+@dataclass
 class AddPurchaseModel():
-
-    def __init__(self, amount: Decimal, currency: str, date: date):
-        self._amount = amount
-        self._currency = currency
-        self._date = date
-
-    @property
-    def amount(self) -> Decimal:
-        return self._amount  # pragma: no cover
-
-    @property
-    def currency(self) -> str:
-        return self._currency  # pragma: no cover
-
-    @property
-    def date(self) -> date:
-        return self._date  # pragma: no cover
-
-    def __eq__(self, other):
-        if not isinstance(other, AddPurchaseModel):
-            return False
-
-        same_amount = self._amount == other._amount
-        same_currency = self._currency == other._currency
-        same_date = self._date == other._date
-
-        return True if (same_amount and same_currency and same_date) else False
+    amount: Decimal
+    currency: str
+    date: date
 
 
 class AddPurchase(ABC):
