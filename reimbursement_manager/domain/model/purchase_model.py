868b9497bc520e29b4859b2719daa89a631eaac3
Victor Mattos <5757883+vicmattos@users.noreply.github.com>
2022-03-16 15:09:15 -0300

    refactor: change PurchaseModel to be a @dataclass

--- reimbursement_manager/domain/model/purchase_model.py
+++ reimbursement_manager/domain/model/purchase_model.py
@@ -1,27 +1,11 @@
+from dataclasses import dataclass
 from datetime import date
 from decimal import Decimal
 
 
+@dataclass
 class PurchaseModel():
-
-    def __init__(self, id: str, amount: Decimal, currency: str, date: date):  # pragma: no cover
-        self._id = id
-        self._amount = amount
-        self._currency = currency
-        self._date = date
-
-    @property
-    def id(self):
-        return self._id  # pragma: no cover
-
-    @property
-    def amount(self) -> Decimal:
-        return self._amount  # pragma: no cover
-
-    @property
-    def currency(self) -> str:
-        return self._currency  # pragma: no cover
-
-    @property
-    def date(self) -> date:
-        return self._date  # pragma: no cover
+    id: str
+    amount: Decimal
+    currency: str
+    date: date
